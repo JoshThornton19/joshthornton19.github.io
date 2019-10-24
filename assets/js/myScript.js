2e2e43e53e268d74d437ef17dc8c15188847f9b5
Josh Thornton <48497154+s3786643@users.noreply.github.com>
2019-10-25 00:47:29 +1100

    add a change background button

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -1,7 +1,12 @@
 function randomBackground() {
 	var bg = ["defaultBG","enableBG1","enableBG2"];
 	var number = (Math.floor(Math.random() * bg.length) + 1)-1;
-	console.log(number);
+
 	document.getElementById('bg').classList.add(bg[number]);
-	console.log(bg[number]);
+
+
+}
+
+function changeBackground() {
+	location.reload();
 }
