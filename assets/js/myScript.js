d0838d90e584f49cfd254bcac923074841322ccb
Josh Thornton <48497154+s3786643@users.noreply.github.com>
2019-10-25 00:38:24 +1100

    remove one of the images

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -1,5 +1,5 @@
 function randomBackground() {
-	var bg = ["defaultBG","enableBG1","enableBG2","enableBG3"];
+	var bg = ["defaultBG","enableBG1","enableBG2"];
 	var number = (Math.floor(Math.random() * bg.length) + 1)-1;
 	console.log(number);
 	document.getElementById('bg').classList.add(bg[number]);
