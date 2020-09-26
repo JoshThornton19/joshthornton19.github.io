5d8786082088a5466e038924e174013e38dc933f
Joshua Thornton <joshdthornton19@gmail.com>
2020-09-26 21:11:48 +1000

    remove picture

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -1,5 +1,5 @@
 function randomBackground() {
-	var bg = ["defaultBG","enableBG1","enableBG2","enableBG3","enableBG4", "enableBG5"];
+	var bg = ["defaultBG","enableBG1","enableBG2","enableBG3","enableBG4"];
 	var number = (Math.floor(Math.random() * bg.length) + 1)-1;
 
 	document.getElementById('bg').classList.add(bg[number]);
